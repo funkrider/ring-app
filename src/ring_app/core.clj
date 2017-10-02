87924f1d474050b62ef4f8e09c96732351c4de0d
funkrider <ian_davies@hotmail.com>
2017-10-02 11:29:21 -0400

    Add ring wrap-reload and no cache handler as middleware.
    * src/ring_app/core.clj:

--- src/ring_app/core.clj
+++ src/ring_app/core.clj
@@ -1,16 +1,23 @@
 (ns ring-app.core
-  (:require [ring.adapter.jetty :as jetty]))
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.util.response :as response]
+            [ring.middleware.reload :refer [wrap-reload]]))
 
 (defn handler [request-map]
   {:status 200
    :headers {"Content-Type" "text/html"}
    :body (str "<html><body> your IP is: "
-              (:remote-add request-map)
+              (:remote-addr request-map)
               "</body></html>")})
 
+(defn wrap-nocache []
+  (fn [request]
+    (-> request
+        handler
+        (assoc-in [:headers "Pragma"] "no-cahce"))))
 
 (defn -main []
   (jetty/run-jetty
-   handler
+   (-> handler var wrap-nocache wrap-reloady)
    {:port 3000
     :join? false})) ;; join sets server blocking. False is required for REPL
