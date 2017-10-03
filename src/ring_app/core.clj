f305e398a6674f6f45a3b76b5d2d92bfde0c70c9
funkrider <ian_davies@hotmail.com>
2017-10-02 20:29:24 -0400

    Add in first compojure routing

--- src/ring_app/core.clj
+++ src/ring_app/core.clj
@@ -2,22 +2,32 @@
   (:require [ring.adapter.jetty :as jetty]
             [ring.util.http-response :as response]
             [ring.middleware.reload :refer [wrap-reload]]
-            [ring.middleware.format :refer [wrap-restful-format]]))
+            [ring.middleware.format :refer [wrap-restful-format]]
+            [compojure.core :as compojure]))
 
-(defn handler-old [request]
+;; curl -H "Content-Type: application/json" -X POST -d "{\"id\" :1}" localhost:3000/json
+;; will result in {"result" 1}
+
+(defn response-handler [request]
   (response/ok
     (str "<html><body> your IP is: "
        (:remote-addr request)
        "</body></html>")))
 
-(defn handler [request]
+(defn api-call-handler [request]
   (response/ok
    {:result (-> request :params :id)}))
+
+(defn handler
+  (compojure/routes
+   (compojure/GET "/" request response-handler)
+   (compojure/GET "/:id" [id] (str "<p>the id is: " id "</p>"))
+   (compojure/POST "/json" [id] (response/ok {:result id}))))    ;; id is expected to be a json encoded param at the root of the json e.g. {"id" :1} etc.
 
 (defn wrap-formats [handler]
   (wrap-restful-format
    handler
-   {formats [:json-kw :transit-json :transit-msgpack]}))
+   {:formats [:json-kw :transit-json :transit-msgpack]}))
 
 (defn wrap-nocache [request]
   (fn [request]
